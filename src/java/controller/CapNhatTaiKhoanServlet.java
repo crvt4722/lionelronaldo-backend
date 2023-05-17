9161c91468b11fc32abea55b87729b3b7e46bfee
Anan27082002 <anan27082002@gmail.com>
2023-05-17 13:28:18 +0700

    update code, servelet

---
+++ src/java/controller/CapNhatTaiKhoanServlet.java
@@ -0,0 +1,56 @@
+package controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import modal.TaiKhoan;
+
+@WebServlet(name = "CapNhatTaiKhoanServlet", urlPatterns = {"/capnhattaikhoanservlet"})
+public class CapNhatTaiKhoanServlet extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, ClassNotFoundException {
+        response.setContentType("text/html;charset=UTF-8");
+        try ( PrintWriter out = response.getWriter()) {
+            
+            HttpSession session = request.getSession();
+            String name = ((String) session.getAttribute("name")).trim();
+            TaiKhoan taikhoan = TaiKhoan.GetTaiKhoan(name);
+            session.setAttribute("TaiKhoan", taikhoan);
+            response.sendRedirect("capnhattaikhoan.jsp");
+            
+        }
+    }
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(CapNhatTaiKhoanServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+            processRequest(request, response);
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(CapNhatTaiKhoanServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+
+}
