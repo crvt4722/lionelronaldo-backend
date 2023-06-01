aeb35745cfc8c194c09be998a24e24f0c0cef6d2
DucAnhDepZai <ducanh25112002tb@gmail.com>
2023-06-01 17:17:46 +0700

    add servlet, DAO

--- src/java/dao/DBConfig.java
+++ src/java/dao/DBConfig.java
@@ -4,6 +4,9 @@
  */
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 /**
  *
  * @author User
@@ -14,5 +17,17 @@ public class DBConfig {
     public static String url = "jdbc:mysql://localhost:3306/mydb";
     public static String user = "root";
     public static String password = "25112002";
+    public static Connection openConnection() {
+        Connection con = null;
+        try {
+//            System.out.println("Hello from DBConfig.driver " + DBConfig.driver);
+            Class.forName(DBConfig.driver);
+            con = DriverManager.getConnection(url, user, password);
+//            System.out.println("connected successfully");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return con;
+    }
 
 }
